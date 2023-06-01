fdf2d5306c99f17de38df9b9dbfca8d2077fe2cc
numberone <qudisajo@student.eursc.eu>
2023-06-01 22:37:29 +0200

    somehow fucked it up

--- src/main.rs
+++ src/main.rs
@@ -1,163 +1,163 @@
 use minifb::*;
 
 fn main() {
-    static mut POINTS: [[f32; 3]; 8] = [
-        [600.0, 200.0, 100.0],  // A
-        [200.0, 200.0, 100.0],  // B
-        [200.0, 200.0, -100.0], // C
-        [600.0, 200.0, -100.0], // D
-        [200.0, 600.0, 100.0],  // E
-        [600.0, 600.0, 100.0],  // F
-        [200.0, 600.0, -100.0], // G
-        [600.0, 600.0, -100.0], // H
-    ];
-    const CONNECTIONS: [[usize; 2]; 12] = [
-        [0, 1],
-        [0, 3],
-        [1, 2],
-        [2, 3],
-        [1, 4],
-        [0, 5],
-        [2, 6],
-        [3, 7],
-        [5, 4],
-        [5, 7],
-        [4, 6],
-        [6, 7],
-    ];
+	static mut POINTS: [[f32; 3]; 8] = [
+		[600.0, 200.0, 100.0],  // A
+		[200.0, 200.0, 100.0],  // B
+		[200.0, 200.0, -100.0], // C
+		[600.0, 200.0, -100.0], // D
+		[200.0, 600.0, 100.0],  // E
+		[600.0, 600.0, 100.0],  // F
+		[200.0, 600.0, -100.0], // G
+		[600.0, 600.0, -100.0], // H
+	];
+	const CONNECTIONS: [[usize; 2]; 12] = [
+		[0, 1],
+		[0, 3],
+		[1, 2],
+		[2, 3],
+		[1, 4],
+		[0, 5],
+		[2, 6],
+		[3, 7],
+		[5, 4],
+		[5, 7],
+		[4, 6],
+		[6, 7],
+	];
 
-    const TOTAL_LINE_LENGTH: usize = 200;
-    static mut LINE_POINTS: [[i32; 2]; TOTAL_LINE_LENGTH] = [[0, 0]; TOTAL_LINE_LENGTH];
+	const TOTAL_LINE_LENGTH: usize = 200;
+	static mut LINE_POINTS: [[i32; 2]; TOTAL_LINE_LENGTH] = [[0, 0]; TOTAL_LINE_LENGTH];
 
-    let theta: f32 = 0.01;
-    let sine_theta: f32 = theta.sin();
-    let cosine_theta: f32 = theta.cos();
+	let theta: f32 = 0.01;
+	let sine_theta: f32 = theta.sin();
+	let cosine_theta: f32 = theta.cos();
 
-    let rotate_x = || unsafe {
-        for mut i in POINTS {
-            i = [
-                i[0],
-                i[1] * cosine_theta + i[2] * -sine_theta,
-                i[1] * sine_theta + i[2] * cosine_theta,
-            ];
-        }
-    };
-    let rotate_y = || unsafe {
-        for mut i in POINTS {
-            i = [
-                i[0] * cosine_theta + i[2] * sine_theta,
-                i[1],
-                i[0] * -sine_theta + i[2] * cosine_theta,
-            ];
-        }
-    };
-    let rotate_z = || unsafe {
-        for mut i in POINTS {
-            i = [
-                i[0] * cosine_theta + i[1] * -sine_theta,
-                i[0] * sine_theta + i[1] * cosine_theta,
-                i[2],
-            ];
-        }
-    };
+	let rotate_x = || unsafe {
+		for mut i in POINTS {
+			i = [
+				i[0],
+				i[1] * cosine_theta + i[2] * -sine_theta,
+				i[1] * sine_theta + i[2] * cosine_theta,
+			];
+		}
+	};
+	let rotate_y = || unsafe {
+		for mut i in POINTS {
+			i = [
+				i[0] * cosine_theta + i[2] * sine_theta,
+				i[1],
+				i[0] * -sine_theta + i[2] * cosine_theta,
+			];
+		}
+	};
+	let rotate_z = || unsafe {
+		for mut i in POINTS {
+			i = [
+				i[0] * cosine_theta + i[1] * -sine_theta,
+				i[0] * sine_theta + i[1] * cosine_theta,
+				i[2],
+			];
+		}
+	};
 
-    const SIDE_LENGTH: u32 = 800;
-    let mut window = Window::new(
-        "magic 🔥🔥",
-        SIDE_LENGTH as usize,
-        SIDE_LENGTH as usize,
-        WindowOptions::default(),
-    )
-    .expect("something happened :(");
+	const SIDE_LENGTH: u32 = 800;
+	let mut window = Window::new(
+		"magic 🔥🔥",
+		SIDE_LENGTH as usize,
+		SIDE_LENGTH as usize,
+		WindowOptions::default(),
+	)
+		.expect("something happened :(");
 
-    const fn from_u8_rgb(r: u8, g: u8, b: u8) -> u32 {
-        // stolen from minifb docs
-        let (r, g, b) = (r as u32, g as u32, b as u32);
-        (r << 16) | (g << 8) | b
-    }
+	const fn from_u8_rgb(r: u8, g: u8, b: u8) -> u32 {
+		// stolen from minifb docs
+		let (r, g, b) = (r as u32, g as u32, b as u32);
+		(r << 16) | (g << 8) | b
+	}
 
-    const WHITE: u32 = from_u8_rgb(255, 255, 255);
-    static BLACK: u32 = from_u8_rgb(0, 0, 0);
+	const WHITE: u32 = from_u8_rgb(255, 255, 255);
+	static BLACK: u32 = from_u8_rgb(0, 0, 0);
 
-    let mut buffer: Vec<u32> = vec![WHITE; (SIDE_LENGTH * SIDE_LENGTH) as usize];
+	let mut buffer: Vec<u32> = vec![WHITE; (SIDE_LENGTH * SIDE_LENGTH) as usize];
 
-    fn round(n: f32) -> u32 {
-        (n + 0.5).floor() as u32
-    }
+	fn round(n: f32) -> u32 {
+		(n + 0.5).floor() as u32
+	}
 
-    let index_pixel = |x: u32, y: u32| unsafe {
-        for mut i in LINE_POINTS {
-            if i == [0, 0] {
-                i = [x as i32, y as i32];
-            };
-            println!("Indexed pixel ({x:?}, {y:?})");
-        }
-    };
+	let index_pixel = |x: u32, y: u32| unsafe {
+		for mut i in LINE_POINTS {
+			if i == [0, 0] {
+				i = [x as i32, y as i32];
+			};
+			println!("Indexed pixel ({x:?}, {y:?})");
+		}
+	};
 
-    // bresenham's line drawing algorithm
-    let drawline = |x0: f32, y0: f32, x1: f32, y1: f32| {
-        let dx = x1 - x0;
-        let dy = y1 - y0;
-        let mut x = x0;
-        let mut y = y0;
-        let mut p = 2.0 * dy - dx;
+	// bresenham's line drawing algorithm
+	let drawline = |x0: f32, y0: f32, x1: f32, y1: f32| {
+		let dx = x1 - x0;
+		let dy = y1 - y0;
+		let mut x = x0;
+		let mut y = y0;
+		let mut p = 2.0 * dy - dx;
 
-        while x < x1 {
-            if p >= 0.0 {
-                index_pixel(round(x), round(y));
-                y += 1.0;
-                p += 2.0 * dy - 2.0 * dx;
-            } else {
-                index_pixel(round(x), round(y));
-                p += 2.0 * dy;
-            };
-            x += 1.0;
-        }
-    };
+		while x < x1 {
+			if p >= 0.0 {
+				index_pixel(round(x), round(y));
+				y += 1.0;
+				p += 2.0 * dy - 2.0 * dx;
+			} else {
+				index_pixel(round(x), round(y));
+				p += 2.0 * dy;
+			};
+			x += 1.0;
+		}
+	};
 
-    let rotate_points = || unsafe {
-        for v in CONNECTIONS {
-            let start = [POINTS[v[0]][0], POINTS[v[0]][1]];
-            let end = [POINTS[v[1]][0], POINTS[v[1]][1]];
+	let rotate_points = || unsafe {
+		for v in CONNECTIONS {
+			let start = [POINTS[v[0]][0], POINTS[v[0]][1]];
+			let end = [POINTS[v[1]][0], POINTS[v[1]][1]];
 
-            drawline(start[0], start[1], end[0], end[1]);
-        }
+			drawline(start[0], start[1], end[0], end[1]);
+		}
 
-        rotate_x();
-        rotate_y();
-        rotate_z();
-    };
+		rotate_x();
+		rotate_y();
+		rotate_z();
+	};
 
-    loop {
-        rotate_points();
-        {
-            buffer = vec![WHITE; (SIDE_LENGTH * SIDE_LENGTH) as usize];
-            window
-                .update_with_buffer(&buffer, SIDE_LENGTH as usize, SIDE_LENGTH as usize)
-                .unwrap();
-        }
+	loop {
+		rotate_points();
+		{
+			buffer = vec![WHITE; (SIDE_LENGTH * SIDE_LENGTH) as usize];
+			window
+				.update_with_buffer(&buffer, SIDE_LENGTH as usize, SIDE_LENGTH as usize)
+				.unwrap();
+		}
 
-        unsafe {
-            for mut i in LINE_POINTS {
-                let x = i[0];
-                let y = i[1];
+		unsafe {
+			for mut i in LINE_POINTS {
+				let x = i[0];
+				let y = i[1];
 
-                if y > 800 || y == 0 {
-                    i = [0, 0];
-                } else {
-                    let index = ((y - 1) * SIDE_LENGTH as i32 + x - 1) as usize;
-                    buffer[index] = BLACK;
-                };
-            }
-            LINE_POINTS = [[0, 0]; TOTAL_LINE_LENGTH];
-        };
+				if y > 800 || y == 0 {
+					i = [0, 0];
+				} else {
+					let index = ((y - 1) * SIDE_LENGTH as i32 + x - 1) as usize;
+					buffer[index] = BLACK;
+				};
+			}
+			LINE_POINTS = [[0, 0]; TOTAL_LINE_LENGTH];
+		};
 
-        window
-            .update_with_buffer(&buffer, SIDE_LENGTH as usize, SIDE_LENGTH as usize)
-            .unwrap();
+		window
+			.update_with_buffer(&buffer, SIDE_LENGTH as usize, SIDE_LENGTH as usize)
+			.unwrap();
 
-        if !window.is_open() {
-            std::process::exit(69);
-        };
-    }
+		if !window.is_open() {
+			std::process::exit(69);
+		};
+	}
 }
